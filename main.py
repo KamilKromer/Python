b3f3a515bccdb954bb1ceaca606ef63b9fd2a67d
Kamil K <enderek72@gmail.com>
2023-01-17 20:06:19 +0100

    Finish the project (Hopefully)

--- main.py
+++ main.py
@@ -1,64 +1,131 @@
 from string_parser import *
+from math import sqrt
 
+default_message = [
+    "Equation calculator, list of operations:",
+    "+    -> add",
+    "-    -> substract",
+    "*    -> multiply",
+    "/    -> divide",
+    "^    -> raise to the power",
+    "R    -> square root",
+    "Brackets ( ) are supported, order of operations applies!",
+    "Remember to insert spaces properly when using the - (minus) symbol, - 9 * 8 is not equal (codewise) to -9 * 8"
+]
 
-def result(res):
-    print(f'Wynik to: {res}')
+message_lock = False
 
 
 def main():
-    rw = input('Wpisz działanie: ')
-    parsed = parse_string(rw)#[2]
-    print(parsed)
-    #print((len(parsed) > 0) and (parsed[len(parsed) - 2] in klucz))
+    global message_lock
+    if not message_lock:
+        message_lock = True
+        for message in default_message:
+            print(message, end = '\n')
+
+    raw = input('Waiting for input: ')
+
+    parsed = parse_string(raw)
     if parsed == False:
-        print('An error occured!')
-        return
+        main()
+    print(f"MAIN: Parsed string: {parsed}")
 
     sequence = check_operation_order(parsed)
+    print(f"MAIN: Sequence is: {sequence}")
 
-    #calculate(parsed, sequence)
-    print(sequence)
+    result = 0
+    try:
+        result = calculate(parsed, sequence)
+    except:
+        print("An error occured while calculating!")
+    print(f"Result: {result}")
+
     main()
 
 
+
+
+#Calculation
 def calculate(parsed: list, operations: list):
+    print(parsed)
     for o_index in range(len(operations)):
-        if type(operations[o_index]) == list:
-            sequence = check_operation_order(parsed[o_index])
-            calculate(parsed[o_index], sequence) # Calculate expressions in brackets
+        print("Operation check!")
+        if type(parsed[operations[o_index]]) == list:
+            print("Operation checked true")
+            sequence = check_operation_order(parsed[operations[o_index]])
+            print(sequence)
+            calculate(parsed[operations[o_index]], sequence) # Calculate expressions in brackets, self recurring code
 
-        # For this operations we take left and right number, then store them and replace them with & character
-        # -1 is left, 1 is right (Direction)
+        for letter_ind in range(len(parsed)):
+                    letter = parsed[letter_ind]
+                    if type(letter) == list:
+                        not_fill_obj = 0
+                        number = 0
+                        for am in parsed[letter_ind]:
+                            if am == '&':
+                                continue
+                            else:
+                                not_fill_obj += 1
+                                number = am
+                        if not_fill_obj == 1: #Theres only one object, which means that this has been calculated and waits to be collapsed into parsed
+                            parsed[letter_ind] = number
 
-        if parsed[o_index] == '*':
-            left = Get_num(parsed, -1, o_index)
-            right = Get_num(parsed, 1, o_index)
 
-            parsed[o_index] = left * right
-        
-        if parsed[o_index] == '/':
-            left = Get_num(parsed, -1, o_index)
-            right = Get_num(parsed, 1, o_index)
+         #For this operations we take left and right number, then store them and replace them with & character
+         #-1 is left, 1 is right (Direction)
 
-            parsed[o_index] = left / right
+        index = operations[o_index]
+        print("Current operation index is " + str(index) + " in parsed equal to ")
+        print(parsed)
+        if parsed[index] == '*':
+            left = Get_num(parsed, -1, index)
+            right = Get_num(parsed, 1, index)
+            print("Left: " + str(left))
+            print("Right: " + str(right))
+            parsed[index] = left * right
+            print("Parsed od index: " + str(parsed[index]))
+            print(parsed)
 
-        if parsed[o_index] == '+':
-            left = Get_num(parsed, -1, o_index)
-            right = Get_num(parsed, 1, o_index)
+        if parsed[index] == '/':
+            left = Get_num(parsed, -1, index)
+            right = Get_num(parsed, 1, index)
 
-            parsed[o_index] = left + right
+            parsed[index] = left / right
 
-        if parsed[o_index] == '-':
-            left = Get_num(parsed, -1, o_index)
-            right = Get_num(parsed, 1, o_index)
+        if parsed[index] == '+':
+            left = Get_num(parsed, -1, index)
+            right = Get_num(parsed, 1, index)
 
-            parsed[o_index] = left - right
-        
-        if parsed[o_index] == '^':
-            left = Get_num(parsed, -1, o_index)
-            right = Get_num(parsed, 1, o_index)
+            parsed[index] = left + right
+        if parsed[index] == '-':
+            left = Get_num(parsed, -1, index)
+            right = Get_num(parsed, 1, index)
+            print("Left: " + str(left))
+            print("Right: " + str(right))
+            parsed[index] = left - right
+            print("Parsed od index: " + str(parsed[index]))
+        if parsed[index] == '^':
+            left = Get_num(parsed, -1, index)
+            right = Get_num(parsed, 1, index)
 
-            parsed[o_index] = left ** right
+            parsed[index] = left ** right
+        if parsed[index] == 'R':
+            right = Get_num(parsed, 1, index)
+
+            parsed[index] = sqrt(right)
+
+    count = 0
+    num = 0
+    for am in parsed:
+        if am == '&':
+            continue
+        else:
+            count += 1
+            num = am
+    print(f"Count is {count}")
+    if count == 1: #Theres only one object, which means that this has been calculated and waits to be collapsed into final result
+        parsed = num #End, return the result
+    return parsed
 
 
 def Get_num(parsed: list, direction: int, start: int):
--- string_parser.py
+++ string_parser.py
@@ -1,6 +1,10 @@
-klucz = ['+', '*', '/']
-klucz_INDEX_ERROR = ['+', '*', '/', ')']
-
+key = ['+', '*', '/', 'R', '^'] #Recognized keywords
+key_INDEX_ERROR = ['+', '*', '/', ')'] #Keywords that cannot exist as a first element, for example *8 + 10
+minus_multiply_operations = ['(', 'R'] #Operations that can be multiplied by -1, for example -(9) = -1 * (9), only one symbol equations!
+self_multiply = ['bracket'] #Operations that can multiply without usage of the "*" symbol, for example (9)(8) = (9) * (8)
+self_multiply_disable_after = ['+', '-', '/', '*', 'R', '^'] #Operations that cannot have a multiply sign after them, for example R * (10 - 9)
+# This is checked in the second object, not the first!
+high_order = ['^', 'R'] #Objects that have a higher priority than multiplying
 
 def parse_string(val):
     recursion_index: int = 0
@@ -12,60 +16,75 @@ def parse_string(val):
     cur_ind: int = -1
     for index in range(len(val)):
         letter = val[index]
+        #print(f'Current letter is: {letter}')
+        #print(f"Parsed is: {parsed}")
+        #print(double_mode)
         if letter == ' ' and not double_mode:
-            print('Minus multiply set to 0!')
+            #print('Minus multiply set to false!')
             minus_multiply = False
             cur_ind = -1
             continue
-
+        elif (letter in minus_multiply_operations and minus_multiply) or len(parsed) == 1 and parsed[0] == '-': # checking for a situation where the minus is in front of the whole equation, like - 9 * 8
+            #print("Replacing minus for multiplication!")
+            parsed.pop() # Remove the minus
+            parsed.append("-1")
+            parsed.append("*")
+            cur_ind = -1 #Minus activates cur_ind, so when we remove it we need to disable it for numeric numbers not to append to this index
+            minus_multiply = False #This too.
         # Start bracket parse
-        elif letter == ')':
+        if letter == ')':
             if recursion_index > 0: #if recursion index is greater than 0, skip
-                print('Recursion index: ' + str(recursion_index))
+                print(f'Recursion index: {recursion_index}')
                 recursion_index -= 1
                 double_parse += letter
                 print(f"Double parse is: {double_parse}")
                 continue
 
             elif double_mode:
-                double_mode == False
-                parsed.append(parse_string(double_parse))
+                double_mode = False
+                parsed_string = parse_string(double_parse)
+                if not parsed_string: #Check for potential errors
+                    return False
+                
+                print(f"Parsing for double parse: {double_parse}")
+                print(f"Parsed double parse: {parsed_string}")
+                parsed.append(parsed_string)
                 double_parse = ""
                 continue
             elif not double_mode:
                 print('Syntax error!')
                 return False
 
-        elif double_mode:
+        if double_mode:
             if letter == '(':
-                print('Recursion in double mode!')
+                #print('Recursion in double mode!')
                 recursion_index += 1
                 
             double_parse += letter
             print(f"Double parse is: {double_parse}")
             continue
 
-        elif letter == '(':  # Enable double parse
+        if letter == '(':  # Enable double parse
             double_mode = True
-            print("Double mode enabled")
+            #print("Double mode enabled")
             continue
         #End bracket parse
         elif letter == '-':
-            if minus_multiply:
+            if minus_multiply or (len(parsed) > 0 and parsed[-1] in key): # Situation where there are two minuses together, or an example situation 10 * - 2
                 print("Syntax error!")
                 return False
-            
-            print('minus multiply set to true!')
+            #print('minus multiply set to true!')
             minus_multiply = True
             parsed.append('-')
-            print(parsed)
-            print(parsed[-1])
+            #print(parsed)
+            #print(parsed[-1])
             cur_ind = len(parsed) - 1
-            print('Cur index is ' + str(cur_ind))
+            #print('Cur index is ' + str(cur_ind))
 
-        elif letter.isnumeric():
+        elif CheckNumeric(letter):
+            #print("After checknumeric parse " + str(parsed))
             if minus_multiply:
-                print("Numeric called, minus is online")
+                #print("Numeric called, minus is online")
                 minus_multiply = False
             if cur_ind > -1:
                 parsed[cur_ind] += letter
@@ -73,40 +92,79 @@ def parse_string(val):
             else:
                 parsed.append(letter)
                 cur_ind = len(parsed) - 1
+                #print(cur_ind)
                 continue
-        
-        elif letter in klucz: #This letter is in klucz
+        elif letter in key or letter == '-': #This letter is in key, we can't add minus to the key directly
             cur_ind = -1
-            if (letter in klucz_INDEX_ERROR and len(parsed) <= 0) or ( (len(parsed) > 0) and (parsed[-1] in klucz) ): #Second statements checks if there isnt a situation  like this: 3 */ 2
+            if (letter in key_INDEX_ERROR and len(parsed) <= 0) or ( (len(parsed) > 0) and (parsed[-1] in key or parsed[-1] == '-') ): #Second statements checks if there isnt a situation  like this: 3 */ 2
                 print('Syntax error!')
                 return False
             else:
                 parsed.append(letter)
                 continue
         else:
-            print('Syntax error!')
+            print(f'Syntax error, {letter} is not defined!')
             return False
+
+    if not CheckForErrors(parsed):
+        return False
+    parsed = InsertMultiplication(parsed)
     parsed = convert_to_float(parsed)
     return parsed
 
 
 def check_operation_order(parsed: list):
     sequence = []
-    for operation in parsed:
+    for operation_ind in range(len(parsed)):
+        operation = parsed[operation_ind]
         if type(operation) == list:
-            sequence.append(parsed.index(operation)) #Make a list in a list
-    for operation in parsed:
-        if operation == '^' or operation == 'root':
-            sequence.append(parsed.index(operation))
-    for operation in parsed:
+            sequence.append(operation_ind)
+
+    for operation_ind in range(len(parsed)):
+        operation = parsed[operation_ind]
+        if operation in high_order:
+            sequence.append(operation_ind)
+
+    for operation_ind in range(len(parsed)):
+        operation = parsed[operation_ind]
         if operation == '*' or operation == '/':
-            sequence.append(parsed.index(operation)) #everything else -> go from left to right
+            sequence.append(operation_ind) #everything else -> go from left to right
+
+    for operation_ind in range(len(parsed)):
+        operation = parsed[operation_ind]
+        if operation == '+' or operation == '-':
+            sequence.append(operation_ind)
+
     return sequence
 
 
 def convert_to_float(val: list):
     for index in range(len(val)):
-        if val[index].isnumeric():
+        if type(val[index]) == list:
+            continue
+        if CheckNumeric(val[index]):
             val[index] = float(val[index])  # Change to float, always
     return val
 
+def CheckNumeric(val):
+    try:
+        float(val)
+        return True
+    except:
+        return False
+
+def CheckForErrors(val: list):
+    last_letter_was_numeric = False
+    for letter in val:
+        if CheckNumeric(letter) and last_letter_was_numeric:
+            print("Syntax Error!")
+            return False
+        last_letter_was_numeric = CheckNumeric(letter)
+    return True
+
+def InsertMultiplication(val: list): #Inserts multiplication, for example (9)(8) = (9) * (8)
+    for index in range(len(val)):
+        letter = val[index]
+        if (type(letter) == list or letter in self_multiply) and index >= 1 and not val[index - 1] in self_multiply_disable_after:
+            val.insert(index, '*')
+    return val
