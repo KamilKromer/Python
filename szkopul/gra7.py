bfd7f2e9ff893900c2fac5b35016502e1de68f65
Kamil <kamil.kusiak@poczta.onet.eu>
2023-03-10 11:07:27 +0100

    fix gra7

--- szkopul/gra7.py
+++ szkopul/gra7.py
@@ -4,16 +4,12 @@ przedzial = int(przedzial[0]), int(przedzial[1])
 wynik = 0
 
 for liczba in range(przedzial[0], przedzial[1]+1):
-    if len(str(liczba)) == 1 and str(liczba)[0] == '7':
-        pass
-    else:
-        if liczba % 7 == 0:
-            wynik += 1
-
-        sum = 0
-        for lit in str(liczba):
-             sum += int(lit)
-        if sum % 7 == 0:
-            wynik += 1
+    if liczba % 7 == 0:
+        wynik += 1
+    suma = 0
+    for lit in str(liczba):
+        suma += int(lit)
+    if suma % 7 == 0 and liczba != 7:
+        wynik += 1
 
 print(wynik)
